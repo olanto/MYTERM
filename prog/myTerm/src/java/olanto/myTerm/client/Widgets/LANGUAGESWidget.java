a3e07db31ad73939a4639254087512bd4f05136e
NizarGhoula <nizarghoula@gmail.com>
2015-04-18 20:35:15 +0200

    changes for interface languages management

--- prog/myTerm/src/java/olanto/myTerm/client/Widgets/LANGUAGESWidget.java
+++ prog/myTerm/src/java/olanto/myTerm/client/Widgets/LANGUAGESWidget.java
@@ -2,9 +2,9 @@
  * ********
  * Copyright © 2013-2014 Olanto Foundation Geneva
  *
- * This file is part of myTELM.
+ * This file is part of myTERM.
  *
- * myTELM is free software: you can redistribute it and/or modify it under the
+ * myTERM is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Affero General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
  * later version.
@@ -15,7 +15,7 @@
  * details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with myTELM. If not, see <http://www.gnu.org/licenses/>.
+ * along with myTERM. If not, see <http://www.gnu.org/licenses/>.
  *
  *********
  */
