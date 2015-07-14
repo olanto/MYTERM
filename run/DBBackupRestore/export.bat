0e1ca07055b4f3af709b4885256d57a4c2ce1ce0
NizarGhoula <nizarghoula@gmail.com>
2015-07-14 11:27:19 +0200

    dump procedure tested and validated

--- run/DBBackupRestore/export.bat
+++ run/DBBackupRestore/export.bat
@@ -1,6 +1,6 @@
 @echo off 
 cd "C:\Program Files\MySQL\MySQL Server 5.6\bin"
-mysqldump --host=localhost --user=root --password --result-file="C:\MYTERM\mysql\dump\myTermDBDump071315.sql" myterm
+mysqldump --default-character-set=utf8 --add-drop-database --add-drop-table --dump-date --add-locks --host=localhost --port=3306 --user=root --password --result-file="C:\MYTERM\mysql\dump\myTermDBDump071315.sql"  --databases myterm
 echo Done!
 pause
 exit
--- run/DBBackupRestore/import.bat
+++ run/DBBackupRestore/import.bat
@@ -1,6 +1,6 @@
 @echo off
 cd "C:\Program Files\MySQL\MySQL Server 5.6\bin"
-mysql --host=localhost --user=root --password -myterm -e "source C:\MYTERM\mysql\dump\myTermDBDump071315.sql;"
+mysql --host=localhost --user=root --port=3306 --password --default-character-set=utf8 --comments < "C:\MYTERM\mysql\dump\myTermDBDump071315.sql"
 echo Done!
 pause
 exit
